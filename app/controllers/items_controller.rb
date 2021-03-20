dccf1b025008e287b41c335059b836a0d07eedb8
makurauo <0118.fujiwara.doro@gmail.com>
2021-03-20 17:15:44 +0900

    ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image).merge(user_id: current_user.id)
+  end
+
 end
