643b4e34250ca732647e87f359affcda3841199b
makurauo <0118.fujiwara.doro@gmail.com>
2021-03-21 17:15:46 +0900

    ActiveHashーcategory

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,20 @@
 class ItemsController < ApplicationController
 
   def index
+    @item = Item.order("category_at DESC")
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :name, :text, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+
 end
