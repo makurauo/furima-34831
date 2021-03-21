74d344665deefd4976b210ae16ceeb5d07a855d5
makurauo <0118.fujiwara.doro@gmail.com>
2021-03-21 17:27:32 +0900

    ActiveHashーcondition

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,5 @@ class Item < ApplicationRecord
   validates :name, :text, presence: true
 
   validates :category_id, numericality: { other_than: 1 }
-
+  validates :condition_id, numericality: { other_than: 1 }
 end
