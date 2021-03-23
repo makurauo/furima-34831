7efa37bdb6e8de1d972be2d4aa05e9f59553940f
makurauo <0118.fujiwara.doro@gmail.com>
2021-03-23 11:12:35 +0900

    訂正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @item = Item.order('category_at DESC')
+    #@item = Item.order('category_at DESC')
   end
 
   def new
