0914edb20cc32d46df56b80b7ca72c44d8432a7d
makurauo <0118.fujiwara.doro@gmail.com>
2021-03-24 15:58:31 +0900

    set_tweet訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_tweet, only: [:show, :edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,11 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
@@ -43,4 +42,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :text, :category_id, :condition_id, :charge_id, :prefecture_id, :delivery_id, :price,
                                  :image).merge(user_id: current_user.id)
   end
+
+def set_tweet
+  @item = Item.find(params[:id])
+end
+
 end
