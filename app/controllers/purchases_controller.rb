aaa58690d361f8052b0401f02da7974050461bc5
makurauo <0118.fujiwara.doro@gmail.com>
2021-03-29 17:46:37 +0900

    フォーム保存機能

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,23 +1,36 @@
 class PurchasesController < ApplicationController
+  #before_action :rack_items, only: :index
 
-def index
-  @purchase_address = PurchaseAddress.new
-end
-
-def create
-  @purchase_address = PurchaseAddress.new(purchase_params)
-  if @purchase_address.valid?
-    @purchase_address.save
-    redirect_to root_path
-  else
-    render :index
-  end
-end
-
-private
-
-  def purchase_params
-    params.require(:purchase_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id)
+  def index
+    @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
   end
 
-end
+  def create
+
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+    def purchase_params
+      params.require(:purchase_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number)
+                                               .merge(user_id: current_user.id,item_id: params[:item_id])
+    end
+
+#def rack_items
+  #   @purchase_address = PurchaseAddress.find(params[:item_id])
+  #   if current_user.id == @purchase_address.user.id
+  #   redirect_to root_path
+  #   end
+  #end
+
+  end
+
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -13,12 +13,10 @@ class PurchaseAddress
     validates :city
     validates :block 
     validates :phone_number, format: {with: /\A\d{10,11}\z/ }
-    validates :purchase_id 
   end
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    
     Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number:phone_number, purchase_id: purchase.id)
   end
 
